620b76386b85f36b968e89d02090eb6eef1456b9
Pablo López <plopesc@gmail.com>
2014-05-07 17:55:50 +0200

    ROOMS-279: Improve explanations for constraints

--- modules/rooms_availability_constraints/js/rooms_availability_constraints_tab.js
+++ modules/rooms_availability_constraints/js/rooms_availability_constraints_tab.js
@@ -6,17 +6,17 @@
         var vals = [];
 
         if ($('.form-item-rooms-availability-range-unit input', context).is(':checked')) {
-          vals.push(Drupal.t('Range constraints <b>enabled</b> at unit level'));
+          vals.push(Drupal.t('Unit constraints enabled'));
         }
         else {
-          vals.push(Drupal.t('Range constraints disabled at unit level'));
+          vals.push(Drupal.t('Unit constraints disabled'));
         }
 
         if ($('.form-item-rooms-availability-range-type input', context).is(':checked')) {
-          vals.push(Drupal.t('Range constraints <b>enabled</b> at unit type level'));
+          vals.push(Drupal.t('Unit type constraints enabled'));
         }
         else {
-          vals.push(Drupal.t('Range constraints disabled at unit type level'));
+          vals.push(Drupal.t('Unit type constraints disabled'));
         }
 
         return vals.join('<br />');
